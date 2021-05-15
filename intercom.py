81f8530fb4f4f71c2442b0357de6f426c30e2cbc
Toshifumi Tsutsumi <rev.to12@gmail.com>
2021-05-15 22:11:40 +0900

    refactoring

--- intercom.py
+++ intercom.py
@@ -1,36 +1,40 @@
-import numpy as np
-import pyaudio
+from typing import List
+from pyaudio import paInt16, PyAudio
 import wave
- 
-WAVE_FILE = "sample.wav"
-INDEX = 0
-NUM_FRAMES = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
 
-def make_wav_file(file="sample.wav", sec=2):
-    pa = pyaudio.PyAudio()
-    stream = pa.open(format = FORMAT,
-                    channels = CHANNELS,
-                    rate = RATE,
-                    input = True,
-                    frames_per_buffer = NUM_FRAMES)
-    
-    print("Now Recording...")
-    chunked_waves = []
-    for _ in range(0, int(RATE / NUM_FRAMES * sec)):
-        chunked_waves.append(stream.read(NUM_FRAMES))
-    stream.close()
-    pa.terminate()
-    print("Recording Finished.")
+FORMAT = paInt16
+CHANNELS = 2    # 1:Monaural, 2:Stereo
+RATE = 44100    # 441.kHz
+CHUNK = 1024
 
-    wavFile = wave.open(WAVE_FILE, 'wb')
-    wavFile.setnchannels(CHANNELS)
-    wavFile.setsampwidth(pa.get_sample_size(FORMAT))
-    wavFile.setframerate(RATE)
-    wavFile.writeframes(b"".join(chunked_waves))
-    wavFile.close()
+def make_wav_file(frames: List, wav_file: str='intercom.wav'):
+    with wave.open(wav_file, 'wb') as wavFile:
+        wavFile.setnchannels(CHANNELS)
+        wavFile.setsampwidth(PyAudio.get_sample_size(FORMAT))
+        wavFile.setframerate(RATE)
+        wavFile.writeframes(b''.join(frames))
+
+def listen(sec: int=2) -> list:
+    try:
+        frames = []
+        p = PyAudio()
+
+        with p.open(format=FORMAT, channels=CHANNELS, rate=RATE,
+                    input=True, frames_per_buffer=CHUNK) as stream:
+            print("Now Recording...")
+
+            max_range = int(RATE / CHUNK * sec)
+            for _ in range(max_range):
+                frames.append(stream.read(CHUNK))
+            print("Recording Finished.")
+        
+        return frames
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        p.terminate()
 
 if __name__ == "__main__":
-    make_wav_file(file="sample.wav", sec=2) 
+    make_wav_file(frames=listen(), wav_file="test.wav") 
