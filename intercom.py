6278d1bf6b99abeb76d8aa400671931640d22416
Toshifumi Tsutsumi <rev.to12@gmail.com>
2021-06-01 21:50:31 +0900

    add DEVICE_INDEX

--- intercom.py
+++ intercom.py
@@ -3,9 +3,10 @@ from pyaudio import paInt16, PyAudio
 import wave
 
 FORMAT = paInt16
-CHANNELS = 2    # 1:Monaural, 2:Stereo
+CHANNELS = 1    # 1:Monaural, 2:Stereo
 RATE = 44100    # 441.kHz
 CHUNK = 1024
+DEVICE_INDEX = 0
 
 def make_wav_file(frames: List, wav_file: str='intercom.wav'):
     with wave.open(wav_file, 'wb') as wavFile:
@@ -20,12 +21,14 @@ def listen(sec: int=2) -> list:
         p = PyAudio()
 
         with p.open(format=FORMAT, channels=CHANNELS, rate=RATE,
-                    input=True, frames_per_buffer=CHUNK) as stream:
+                    input=True, input_device_index=DEVICE_INDEX,
+                    frames_per_buffer=CHUNK) as stream:
             print("Now Recording...")
 
             max_range = int(RATE / CHUNK * sec)
             for _ in range(max_range):
                 frames.append(stream.read(CHUNK))
+
             print("Recording Finished.")
         
         return frames
